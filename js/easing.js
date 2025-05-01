ba0b29ae50c0e9c382b7fdd5b07e96dbd9f99281
Yuetao <u7748559@anu.edu.au>
2025-05-01 20:21:26 +1000

    web update

--- js/easing.js
+++ js/easing.js
@@ -139,3 +139,40 @@ jQuery.extend( jQuery.easing,
 	}
 });
 
+function transitionToChat() {
+    const input = document.getElementById("initialInput");
+    const question = input.value.trim();
+    if (question === "") return;
+
+     window.location.href = `web_sub_AI_trail.html?question=${encodeURIComponent(question)}`;
+
+    setTimeout(() => {
+      addMessage(question, "user");
+      addMessage("The model is not implemented yet", "bot");
+    }, 600);
+}
+
+function sendMessage() {
+    const input = document.getElementById("userInput");
+    const message = input.value.trim();
+    if (message === "") return;
+
+    addMessage(message, "user");
+    input.value = "";
+
+    setTimeout(() => {
+      addMessage("The model is not implemented yet", "bot");
+    }, 500);
+}
+function addMessage(text, sender) {
+    const chat = document.getElementById("chat");
+    const msgDiv = document.createElement("div");
+    msgDiv.className = `message ${sender}`;
+    msgDiv.textContent = text;
+    chat.appendChild(msgDiv);
+    chat.scrollTop = chat.scrollHeight;
+}
+function goBack() {
+    document.getElementById("page1").style.transform = "translateY(0)";
+    document.getElementById("page2").style.transform = "translateY(100%)";
+}
